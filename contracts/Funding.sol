930966caf40dc3910e4abfdd34c71fb4b59e6020
Prabinbaral77 <prabin.baral00@gmail.com>
2022-05-03 22:18:05 +0545

    smart contract updated

---
+++ contracts/Funding.sol
@@ -0,0 +1,86 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.0 <0.9.0;
+
+contract CrowedFunding {
+    mapping(address => uint) public contributer;
+    address public manager;
+    uint public minContribution;
+    uint public target;
+    uint public deadline;
+    uint public noOfContributer;
+    uint public raisedAmount;
+
+    struct Request {
+        string description;
+        address payable recipient;
+        uint noOfVoters;
+        bool completed;
+        uint value;
+        mapping(address => bool) voters;
+    }
+
+    mapping(uint => Request) public requests;
+    uint public noOfRequests;
+
+    constructor(uint _target, uint _deadline) {
+        manager = msg.sender;
+        deadline = block.timestamp + _deadline;
+        target = _target;
+        minContribution = 100 wei;
+    }
+
+    function sendEth() public payable {
+        require(block.timestamp < deadline, "You cannot donate after deadline.");
+        require(msg.value > minContribution, "Minimum contribution of 100 wei required.");
+        if(contributer[msg.sender] == 0) {
+            noOfContributer += 1;
+        }
+        contributer[msg.sender] += msg.value;
+        raisedAmount += msg.value;
+    }
+
+
+    function getContractBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function refund() public {
+        require(block.timestamp > deadline && raisedAmount < target, "You are not eligible to refund.");
+        require(contributer[msg.sender] > 0, "You didnot contribute for this project.");
+        address payable user = payable(msg.sender);
+        user.transfer(contributer[msg.sender]);
+        contributer[msg.sender] = 0;
+    }
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only manager can call this function");
+        _;
+    }
+
+    function createRequest(string memory _description, address payable _recipient, uint _value) public onlyManager {
+        Request storage newRequest = requests[noOfRequests];
+        noOfRequests ++;
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
+    }
+
+    function voteRequest(uint _requestNo) public {
+        require(contributer[msg.sender] > 0, "You must contribute to vote");
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.voters[msg.sender] == false, "You have already voted.");
+        thisRequest.voters[msg.sender] = true;
+        thisRequest.noOfVoters ++;
+    }
+
+    function makePayment(uint _requestNo) public {
+        require(raisedAmount >= target, "Required Amount is not raised.");
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.completed == false, "The request has already been completed.");
+        require(thisRequest.noOfVoters > noOfContributer / 2, "Majority vote is required to proceed payment.");
+        thisRequest.recipient.transfer(thisRequest.value);
+        thisRequest.completed = true;
+    }
+}
